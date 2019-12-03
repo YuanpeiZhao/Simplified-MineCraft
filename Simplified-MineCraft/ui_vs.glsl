10f511d9e322d7a244e6eaf203187267e61fc32c
Yuanpei Zhao <yuanpeizhao1106@gmail.com>
2019-12-02 22:56:55 -0500

    Debug

--- Simplified-MineCraft/ui_vs.glsl
+++ Simplified-MineCraft/ui_vs.glsl
@@ -1,6 +1,6 @@
 #version 400
-uniform mat4 M;
-
+uniform mat4 T;
+uniform mat4 S;
 layout(location = 0) in vec3 pos_attrib;
 layout(location = 1) in vec2 tex_coord;
 layout(location = 2) in float type;
@@ -12,11 +12,11 @@ void main(void){
 	
 	Type = type;
 	TexCoord = tex_coord;
-
+	
 	if(type == 0.0f){
-		gl_Position = vec4(pos_attrib, 1.0f);
+		gl_Position = S * vec4(pos_attrib, 1.0f);
 	}
 	else{
-		gl_Position = M * vec4(pos_attrib, 1.0f);
+		gl_Position = T * S * vec4(pos_attrib, 1.0f);
 	}
 }
